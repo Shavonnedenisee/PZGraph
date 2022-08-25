ad6c035ab100ac3964f2a9f183515034ed926e4a
Shavonne Cobb <112039829+Shavonnedenisee@users.noreply.github.com>
2022-08-24 22:37:24 -0500

    Testing (#2)
    * one more git testing
    
    * last test

--- src/Components/Page/Body/Body.tsx
+++ src/Components/Page/Body/Body.tsx
@@ -3,6 +3,9 @@ import { LineGraph } from "../../Graph/LineGraph"
 export const Body = (): JSX.Element => {
      return (
           <div>
+               <div>
+                    Power Zones Graph:
+               </div>
                <LineGraph />
           </div>
      )
